5398519f9d8c52ec34ebf12b7c535ec56551e616
Anna K <cellin@yandex.ru>
2026-03-07 00:50:17 +0300

    try_version

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,44 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
 
+const compare = createComparison(defaultRules);
+
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes) // Получаем ключи из объекта
+    .forEach((elementName) => {
+      // Перебираем по именам
+      elements[elementName].append(
+        // в каждый элемент добавляем опции
+        ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
+          .map((name) => {
+            // используйте name как значение и текстовое содержимое
+            const option = document.createElement("option");
+            option.value = name;
+            option.textContent = name; // @todo: создать и вернуть тег опции <option value="name">name</option>
+            const select = document.getElementById("filter");
+            return select.appendChild(option);
+          }),
+      );
+    });
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    const buttons = document.querySelectorAll('button[name="clear"]');
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+    buttons.forEach((button) => {
+      const parent = button.parentElement;
+      const inp = parent.querySelector("input");
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
-    }
+      button.addEventListener("click", () => {
+        //action - ?
+        inp.value = "";
+        state[button.dataset.field] = "";
+      });
+    });
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+
+    return data.filter((row) => compare(row, state));
+  };
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,37 @@
-import './fonts/ys-display/fonts.css'
-import './style.css'
+import "./fonts/ys-display/fonts.css";
+import "./style.css";
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { data as sourceData } from "./data/dataset_1.js";
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
+import { initData } from "./data.js";
+import { processFormData } from "./lib/utils.js";
 
-import {initTable} from "./components/table.js";
+import { initTable } from "./components/table.js";
 // @todo: подключение
-
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const { data, ...indexes } = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
-
-    return {
-        ...state
-    };
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage); // приведём количество страниц к числу
+  const page = parseInt(state.page ?? 1); // номер страницы по умолчанию 1 и тоже число
+  const from = parseFloat(state.totalFrom);
+  const to = parseFloat(state.totalTo);
+  return {
+    ...state,
+    rowsPerPage,
+    page,
+    total: [from, to],
+  };
 }
 
 /**
@@ -30,25 +39,60 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+  let state = collectState(); // состояние полей из таблицы
+  let result = [...data]; // копируем для последующего изменения
+  // @todo: использование
 
+  result = applySearching(result, state, action);
 
-    sampleTable.render(result)
+  result = applyFiltering(result, state, action); //фильтрация до сортировки
+
+  result = applySorting(result, state, action); //сортировка до пагинации
+
+  result = applyPagination(result, state, action);
+
+  sampleTable.render(result);
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: [],
-    after: []
-}, render);
+const sampleTable = initTable(
+  {
+    tableTemplate: "table", // из html
+    rowTemplate: "row", // из html
+    before: ["search", "header", "filter"],
+    after: ["pagination"],
+  },
+  render,
+);
 
 // @todo: инициализация
 
+const applyPagination = initPagination(
+  sampleTable.pagination.elements, // передаём сюда элементы пагинации, найденные в шаблоне
+  (el, page, isCurrent) => { // колбэк, чтобы заполнять кнопки страниц данными
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  },
+);
 
-const appRoot = document.querySelector('#app');
+const applySorting = initSorting([ 
+  sampleTable.header.elements.sortByDate, // передаем массив элементов, которые вызывают сортировку, 
+  sampleTable.header.elements.sortByTotal, //чтобы изменять их визуальное представление
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, { // в аргументах элементы фильтра
+  searchBySeller: indexes.sellers, // для элемента с именем searchBySeller устанавливаем массив продавцов (для выпадающего списка)
+});
+
+const searchInput = sampleTable.search.elements.search;
+const searchName = searchInput.getAttribute("name");
+
+const applySearching = initSearching(searchName);
+
+const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
 render();
